5238b308134dd82534b6a97539569c4db9543f51
Skwhyez <152044509+Skwhyez@users.noreply.github.com>
2026-07-05 11:19:32 +0000

    rip gitpod vibe coded with 113 commits

---
+++ Server/CMakeFiles/gardn-server.dir/DependInfo.cmake
@@ -0,0 +1,61 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspaces/gardn/Server/Client.cc" "CMakeFiles/gardn-server.dir/Client.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/Client.cc.obj.d"
+  "/workspaces/gardn/Server/EntityFunctions/Damage.cc" "CMakeFiles/gardn-server.dir/EntityFunctions/Damage.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/EntityFunctions/Damage.cc.obj.d"
+  "/workspaces/gardn/Server/EntityFunctions/Death.cc" "CMakeFiles/gardn-server.dir/EntityFunctions/Death.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/EntityFunctions/Death.cc.obj.d"
+  "/workspaces/gardn/Server/EntityFunctions/Detection.cc" "CMakeFiles/gardn-server.dir/EntityFunctions/Detection.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/EntityFunctions/Detection.cc.obj.d"
+  "/workspaces/gardn/Server/EntityFunctions/Misc.cc" "CMakeFiles/gardn-server.dir/EntityFunctions/Misc.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/EntityFunctions/Misc.cc.obj.d"
+  "/workspaces/gardn/Server/Game.cc" "CMakeFiles/gardn-server.dir/Game.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/Game.cc.obj.d"
+  "/workspaces/gardn/Server/Main.cc" "CMakeFiles/gardn-server.dir/Main.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/Main.cc.obj.d"
+  "/workspaces/gardn/Server/Native.cc" "CMakeFiles/gardn-server.dir/Native.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/Native.cc.obj.d"
+  "/workspaces/gardn/Server/PetalTracker.cc" "CMakeFiles/gardn-server.dir/PetalTracker.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/PetalTracker.cc.obj.d"
+  "/workspaces/gardn/Server/Process/Ai.cc" "CMakeFiles/gardn-server.dir/Process/Ai.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/Process/Ai.cc.obj.d"
+  "/workspaces/gardn/Server/Process/Camera.cc" "CMakeFiles/gardn-server.dir/Process/Camera.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/Process/Camera.cc.obj.d"
+  "/workspaces/gardn/Server/Process/Chat.cc" "CMakeFiles/gardn-server.dir/Process/Chat.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/Process/Chat.cc.obj.d"
+  "/workspaces/gardn/Server/Process/Collision.cc" "CMakeFiles/gardn-server.dir/Process/Collision.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/Process/Collision.cc.obj.d"
+  "/workspaces/gardn/Server/Process/Culling.cc" "CMakeFiles/gardn-server.dir/Process/Culling.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/Process/Culling.cc.obj.d"
+  "/workspaces/gardn/Server/Process/Curse.cc" "CMakeFiles/gardn-server.dir/Process/Curse.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/Process/Curse.cc.obj.d"
+  "/workspaces/gardn/Server/Process/Drop.cc" "CMakeFiles/gardn-server.dir/Process/Drop.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/Process/Drop.cc.obj.d"
+  "/workspaces/gardn/Server/Process/Flower.cc" "CMakeFiles/gardn-server.dir/Process/Flower.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/Process/Flower.cc.obj.d"
+  "/workspaces/gardn/Server/Process/FlowerAi.cc" "CMakeFiles/gardn-server.dir/Process/FlowerAi.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/Process/FlowerAi.cc.obj.d"
+  "/workspaces/gardn/Server/Process/Health.cc" "CMakeFiles/gardn-server.dir/Process/Health.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/Process/Health.cc.obj.d"
+  "/workspaces/gardn/Server/Process/Motion.cc" "CMakeFiles/gardn-server.dir/Process/Motion.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/Process/Motion.cc.obj.d"
+  "/workspaces/gardn/Server/Process/Petal.cc" "CMakeFiles/gardn-server.dir/Process/Petal.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/Process/Petal.cc.obj.d"
+  "/workspaces/gardn/Server/Process/Score.cc" "CMakeFiles/gardn-server.dir/Process/Score.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/Process/Score.cc.obj.d"
+  "/workspaces/gardn/Server/Process/Segment.cc" "CMakeFiles/gardn-server.dir/Process/Segment.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/Process/Segment.cc.obj.d"
+  "/workspaces/gardn/Server/Server.cc" "CMakeFiles/gardn-server.dir/Server.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/Server.cc.obj.d"
+  "/workspaces/gardn/Server/Simulation.cc" "CMakeFiles/gardn-server.dir/Simulation.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/Simulation.cc.obj.d"
+  "/workspaces/gardn/Server/SpatialHashUniform.cc" "CMakeFiles/gardn-server.dir/SpatialHashUniform.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/SpatialHashUniform.cc.obj.d"
+  "/workspaces/gardn/Server/Spawn.cc" "CMakeFiles/gardn-server.dir/Spawn.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/Spawn.cc.obj.d"
+  "/workspaces/gardn/Server/TeamManager.cc" "CMakeFiles/gardn-server.dir/TeamManager.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/TeamManager.cc.obj.d"
+  "/workspaces/gardn/Helpers/Math.cc" "CMakeFiles/gardn-server.dir/workspaces/gardn/Helpers/Math.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/workspaces/gardn/Helpers/Math.cc.obj.d"
+  "/workspaces/gardn/Helpers/UTF8.cc" "CMakeFiles/gardn-server.dir/workspaces/gardn/Helpers/UTF8.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/workspaces/gardn/Helpers/UTF8.cc.obj.d"
+  "/workspaces/gardn/Helpers/Vector.cc" "CMakeFiles/gardn-server.dir/workspaces/gardn/Helpers/Vector.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/workspaces/gardn/Helpers/Vector.cc.obj.d"
+  "/workspaces/gardn/Shared/Arena.cc" "CMakeFiles/gardn-server.dir/workspaces/gardn/Shared/Arena.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/workspaces/gardn/Shared/Arena.cc.obj.d"
+  "/workspaces/gardn/Shared/Binary.cc" "CMakeFiles/gardn-server.dir/workspaces/gardn/Shared/Binary.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/workspaces/gardn/Shared/Binary.cc.obj.d"
+  "/workspaces/gardn/Shared/Config.cc" "CMakeFiles/gardn-server.dir/workspaces/gardn/Shared/Config.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/workspaces/gardn/Shared/Config.cc.obj.d"
+  "/workspaces/gardn/Shared/Entity.cc" "CMakeFiles/gardn-server.dir/workspaces/gardn/Shared/Entity.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/workspaces/gardn/Shared/Entity.cc.obj.d"
+  "/workspaces/gardn/Shared/EntityDef.cc" "CMakeFiles/gardn-server.dir/workspaces/gardn/Shared/EntityDef.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/workspaces/gardn/Shared/EntityDef.cc.obj.d"
+  "/workspaces/gardn/Shared/Map.cc" "CMakeFiles/gardn-server.dir/workspaces/gardn/Shared/Map.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/workspaces/gardn/Shared/Map.cc.obj.d"
+  "/workspaces/gardn/Shared/Simulation.cc" "CMakeFiles/gardn-server.dir/workspaces/gardn/Shared/Simulation.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/workspaces/gardn/Shared/Simulation.cc.obj.d"
+  "/workspaces/gardn/Shared/StaticData.cc" "CMakeFiles/gardn-server.dir/workspaces/gardn/Shared/StaticData.cc.obj" "gcc" "CMakeFiles/gardn-server.dir/workspaces/gardn/Shared/StaticData.cc.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
